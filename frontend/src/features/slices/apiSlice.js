b2227baa62ad410f1e1b8fbc726558affea8bd05
Scott L <ehee27@gmail.com>
2023-12-28 14:56:20 -0600

    removed renderURL

--- frontend/src/features/slices/apiSlice.js
+++ frontend/src/features/slices/apiSlice.js
@@ -4,7 +4,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://baseballcardz-api.onrender.com/',
+  baseUrl: '',
 })
 
 export const apiSlice = createApi({
