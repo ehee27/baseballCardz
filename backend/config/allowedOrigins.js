56e308a1c81364052fabd4014784fa1d9355243f
Scott L <ehee27@gmail.com>
2023-12-28 14:43:34 -0600

    initialized cors

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,7 @@
+export const allowedOrigins = [
+  'http://localhost:3000',
+  'https://baseballcardz-api.onrender.com',
+  'https://www.baseballcardz-api.onrender.com',
+]
+
+export default allowedOrigins
--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,15 @@ import playerRoutes from './routes/playersRoutes.js'
 import coachRoutes from './routes/coachRoutes.js'
 import connectDB from './dbConfig/db.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import corsOptions from './config/corsOptions.js'
 
 connectDB()
 
 const app = express()
 const port = process.env.PORT || 3500
 
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
