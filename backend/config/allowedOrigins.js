99e9c53b15bce60ab241576191d743401698b531
Scott L <ehee27@gmail.com>
2023-12-28 15:03:33 -0600

    againzzz

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 export const allowedOrigins = [
   'http://localhost:3000',
-  'https://baseballcardz27.com/',
-  'https://www.baseballcardz27.com/',
+  'https://baseballcardz27.com',
+  'https://www.baseballcardz27.com',
 ]
 
 export default allowedOrigins
